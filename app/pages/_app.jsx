a561d03bf98a263923c87de96fa0f19743a6e4dc
Matías Surdi <matiassurdi@gmail.com>
2021-02-13 09:35:04 +0100

    Add manifest and favicon

--- app/pages/_app.jsx
+++ app/pages/_app.jsx
@@ -7,6 +7,10 @@ const WebalizerApp = ({ Component, pageProps }) => (
   <>
     <Head>
       <title>Webalizer</title>
+      <link rel="manifest" href="/manifest.json" />
+      <meta name="theme-color" content="#581c87" />
+      <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      <meta name="apple-mobile-web-app-status-bar" content="#581c87" />
       <meta
         name="viewport"
         content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
