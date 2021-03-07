0caf43fbe32c23f1b9b8b3f65182df5d4c8fca06
Matías Surdi <matiassurdi@gmail.com>
2021-03-07 09:09:47 +0100

    Clear input before typing

--- acceptance/support/commands.js
+++ acceptance/support/commands.js
@@ -7,8 +7,8 @@ Cypress.Commands.add("login", (username, password) => {
     }
   });
 
-  cy.get("#username").type(username);
-  cy.get("#password").type(password);
+  cy.get("#username").clear().type(username);
+  cy.get("#password").clear().type(password);
 
   cy.contains("Sign in").click();
 });
